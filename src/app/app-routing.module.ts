24be3e85dcf7770194b4f56a267d887f33587557
carloskauan <carloskauan190@gmail.com>
2021-09-16 14:30:38 -0300

    Update Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CadastrarProdutosComponent } from './components/produtos/cadastrar-produtos/cadastrar-produtos.component';
+import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
 
-const routes: Routes = [];
+//criar Rotas
+const routes: Routes = [
+
+  {path:'', component:HomeComponent},
+  {path:'produtos', component:ListarProdutosComponent},
+  {path:'produtos/cadastrar', component:CadastrarProdutosComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
