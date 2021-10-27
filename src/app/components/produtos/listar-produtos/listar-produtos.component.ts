fe37f68d6c03d3e638931f24f4a0dbf3587d7989
carloskauan <carloskauan190@gmail.com>
2021-10-27 19:57:44 -0300

    Aprendendo sobre arrays [Primeiros exemplos]

--- src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarProdutosComponent implements OnInit {
 
-  constructor() { }
+  //Criando listas arrays
+  listaStrings: string[] = ['Primeiro','Segundo','Terceiro'];
+  listaNumber: number[] = [1,2,3]
+
+  constructor() {
+    //Laço faça
+    for (let item of this.listaStrings){
+      //Exibir posições
+      console.log(item);
+    }
+  }
 
   ngOnInit(): void {
+
   }
 
 }
